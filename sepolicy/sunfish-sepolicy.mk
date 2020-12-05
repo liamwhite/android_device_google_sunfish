48c79200cdcff96a3f531165a9593328362dbe8d
byte[] <byteslice@airmail.cc>
2020-12-05 00:33:46 -0500

    update sunfish-sepolicy to android-11.0.0_r17

--- sepolicy/sunfish-sepolicy.mk
+++ sepolicy/sunfish-sepolicy.mk
@@ -1,9 +1,13 @@
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/sunfish/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/sunfish/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/sunfish-sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/sunfish-sepolicy/private
 
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy/vendor/qcom/sm7150
-BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy/tracking_denials
-BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy/vendor/st
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/qcom/sm7150
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/st
+BOARD_SEPOLICY_DIRS += device/google/sunfish-sepolicy/vendor/verizon
+
+# Pixel-wide sepolicy
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
