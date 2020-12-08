3542238afe305f4bb28280e4bbad03004d63edca
byte[] <byteslice@airmail.cc>
2020-12-07 22:54:20 -0500

    first build
    Change-Id: Ib769e1a8731c444989cf8cc07f77640cd7a37301

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -114,7 +114,7 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
 
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp
+#BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp
 BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
 
 include device/google/sunfish/sepolicy/sunfish-sepolicy.mk
